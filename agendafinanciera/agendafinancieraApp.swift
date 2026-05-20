01a12edc720eb5e787e20cf8be56d819d72c4228
ramf99dev <molinafloresrandy99@gmail.com>
2026-05-20 08:59:55 -0400

    Initial Commit

---
+++ agendafinanciera/agendafinancieraApp.swift
@@ -0,0 +1,17 @@
+//
+//  agendafinancieraApp.swift
+//  agendafinanciera
+//
+//  Created by Randy Molina on 20/5/26.
+//
+
+import SwiftUI
+
+@main
+struct agendafinancieraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
